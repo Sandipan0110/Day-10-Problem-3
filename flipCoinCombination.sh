7aed3898149ca0da11c78127a319a1cd4f0dbaef
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-27 13:46:33 +0530

    double combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,31 +1,23 @@
 #!/bin/bash -x
 
-declear -A flipValue
-isFlip=0;
-isHead=0;
-isTail=1;
-a=0;
-b=0;
-read -p "Enter how many times you want to flip the coin : " flipCoinTimes
+isHH=0;
+isHT=1;
+isTH=2;
+isTT=3;
+flipCheck=$((RANDOM%4))
 
-while  [[ isFlip -lt flipCoinTimes ]]
-do
-	flipCoin=$(( RANDOM%2 ))
-	case $flipCoin in
-        	$isHead)
-                	result=Heads
-			((a++))
-                	;;
-        	$isTail)
-                	result=Tails
-			((b++))
-                	;;
-	esac
-	flipValue[$result]=$flipCoin
-	echo $result
-	isFlip=$(( $isFlip + 1 ))
-done
-
-echo "${!flipValue[@]}"
-echo "Heads win $a Times"
-echo "Tails win $b Times"
+case $flipCheck in
+	$isHH)
+		result=HH
+		;;
+	$isHT)
+		result=HT
+		;;
+        $isTH)
+                result=TH
+                ;;
+        $isTT)
+                result=TT
+                ;;
+esac
+echo $result "is the winner"
