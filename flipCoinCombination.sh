e8a749c3d4fad56b9beb693a9fcd1754ed557d23
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-27 13:49:05 +0530

    Triple Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,23 +1,40 @@
 #!/bin/bash -x
 
-isHH=0;
-isHT=1;
-isTH=2;
-isTT=3;
-flipCheck=$((RANDOM%4))
+isHHH=0;
+isHHT=1;
+isHTH=2;
+isHTT=3;
+isTHH=4;
+isTHT=5;
+isTTH=6;
+isTTT=7;
+
+flipCheck=$((RANDOM%8))
 
 case $flipCheck in
-	$isHH)
-		result=HH
+	$isHHH)
+		result=HHH
 		;;
-	$isHT)
-		result=HT
+	$isHHT)
+		result=HHT
 		;;
-        $isTH)
-                result=TH
+        $isHTH)
+                result=HTH
                 ;;
-        $isTT)
-                result=TT
+        $isHTT)
+                result=HTT
+                ;;
+        $isTHH)
+                result=THH
+                ;;
+        $isTHT)
+                result=THT
+                ;;
+        $isTTH)
+                result=TTH
+                ;;
+        $isTTT)
+                result=TTT
                 ;;
 esac
 echo $result "is the winner"
