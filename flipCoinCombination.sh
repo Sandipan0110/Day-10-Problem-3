6c9ec608e50822a4aa0f7cd340b13ad6f21fbafc
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-27 13:39:56 +0530

    Display Fliping

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,15 @@
-echo "Start Day 10 Problem 3"
+#!/bin/bash -x
+
+isHead=0;
+isTail=1;
+flipCheck=$((RANDOM%2))
+
+case $flipCheck in
+	$isHead)
+		result=Heads
+		;;
+	$isTail)
+		result=Tails
+		;;
+esac
+echo $result "is the winner"
